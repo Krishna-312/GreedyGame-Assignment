afb1c947e3dc67b68701fcda9f8f98402bb791db
Krishna-312 <90499348+Krishna-312@users.noreply.github.com>
2022-12-28 12:38:36 +0530

    Update BA-Assignment.py

--- BA-Assignment.py
+++ BA-Assignment.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import numpy as np
+
+## Problem Statement -2 
 user_signup = pd.read_csv("/content/drive/MyDrive/GreedyGame Assignment/Q2_users signup.csv")
 
 user_offer = pd.read_csv("/content/drive/MyDrive/GreedyGame Assignment/Q2_User offer data.csv")
@@ -10,20 +14,17 @@ rewards = pd.read_csv("/content/drive/MyDrive/GreedyGame Assignment/Q2_rewards d
 rewards = rewards.drop(['Unnamed: 0'],axis=1)
 
 a = pd.merge(user_signup,user_offer)
-
 a['offer_status'] = a.status.apply(lambda x: 1 if x=='COMPLETED' else 0)
 
-b = pd.merge(offer_completion,rewards)
-
 offers = a.groupby('app_id').agg(offers_initiated = ('offer_status',lambda x: x.eq(0).sum()),
-                                  unique_users = ('user_id',pd.Series.nunique)).reset_index()
+                                        unique_users = ('user_id',pd.Series.nunique)).reset_index()
 
+b = pd.merge(offer_completion,rewards)
 rewards_earned = b.groupby('app_id').agg(offer_completion = ('offer_id','count'),
                                          rewards_amount = ('total_payout_in_paise','sum'),
                                          revenue = ('total_revenue_in_paise','sum')).reset_index()
 
 final = pd.merge(offers,rewards_earned)
-
 final['initiated_completion'] = round((final.offer_completion / final.offers_initiated)*100,2)
 
 final
